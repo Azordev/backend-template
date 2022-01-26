95931d58540eb5a4bdff05ad030906cb280efec8
Emmanuel Azócar <azocarmel@gmail.com>
2022-01-26 14:05:34 -0400

    added test

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,6 @@
-import express from "express";
-import routes from "./routes";
-import useMiddlewares from "./middlewares";
-
-// App
-const app = express();
-
-// Middlewares
-useMiddlewares(app);
-
-// Routes
-app.use("/", routes);
-
-// Port
-const port = process.env.PORT || 3000;
+import app from "./app";
 
 // Server
-app.listen(port, () => {
-  console.log("Server is running on port " + port);
+app.listen(app.get("port"), () => {
+  console.log("Server is running on port", app.get("port"));
 });

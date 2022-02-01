52d640d6e2c13bcb327b4bca9deb389f95a33047
Emmanuel Azócar <61360270+e-azocar@users.noreply.github.com>
2022-02-01 02:53:13 -0400

    Connected to PostgreSQL db (#20)
    * Connected to PostgreSQL db
    
    * removed yarn.lock
    
    * removed sequelize

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,16 @@
 import express from "express";
 import routes from "./routes";
 import useMiddlewares from "./middlewares";
+import { pool } from "./config/database";
 
 // App
 const app = express();
 
 // Settings
 app.set("port", process.env.PORT || 3000);
+pool.connect(() => {
+  console.log("Connected to the database");
+});
 
 // Middlewares
 useMiddlewares(app);
--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,7 +1,16 @@
 import { Router } from "express";
+import { pool } from "../../config/database";
 
 const router = Router();
 
-// Empty routes for now
+router.get("/", async (req, res) => {
+  try {
+    const { rows } = await pool.query("SELECT * FROM users");
+    res.json(rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({});
+  }
+});
 
 export default router;

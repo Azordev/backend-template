64572a0399b29da51cdbdbee577a0ee2b929403f
Israel-Laguan <israellaguan@gmail.com>
2022-02-11 09:37:47 -0500

    feat: add base for test env

---
+++ ormconfig.js
@@ -0,0 +1,2 @@
+const { DB_CONFIG } = require('./build/src/server/environments/database')
+module.exports = DB_CONFIG
---
+++ scripts/clean-package.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+rm -rf ./node_modules
+rm -rf ./coverage
+rm -rf ./build
+rm -rf ./logs

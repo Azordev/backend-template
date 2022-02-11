2326afd81e9fd9c400f5b194b52006f057c0e519
Israel-Laguan <israellaguan@gmail.com>
2022-02-11 10:26:39 -0500

    fix: improve limiter on route status

--- src/api/system/index.ts
+++ src/api/system/index.ts
@@ -3,10 +3,11 @@ import { authenticate } from '../../middlewares/auth'
 import cache from '../../middlewares/cache'
 import ping from '../../controllers/api.ping'
 import status from '../../controllers/api.status'
+import limiter from 'middlewares/limiter'
 
 const system = express.Router()
 
 system.get('/ping', cache(10), ping)
-system.get('/status', cache(1), authenticate, status)
+system.get('/status', cache(1), limiter, authenticate, status)
 
 export default system
